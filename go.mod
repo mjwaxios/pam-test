71ee9d8c1066d20f8b74481b100eb19a9f9ca86c
mwyrick <mjw@axiosengineering.com>
2020-07-16 12:17:06 -0400

    Updated to new versions

--- go.mod
+++ go.mod
@@ -5,7 +5,8 @@ go 1.14
 replace github.com/mjwaxios/axiospam => /home/mwyrick/go/src/github.com/mjwaxios/axiospam
 
 require (
-	github.com/mjwaxios/axiospam v0.2.4
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/mjwaxios/axiospam v0.2.9
+	github.com/mjwaxios/promptuser v0.1.0
+	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899 // indirect
 	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae // indirect
 )
--- main.go
+++ main.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 
 	"github.com/mjwaxios/axiospam"
+	"github.com/mjwaxios/promptuser"
 )
 
 func main() {
-	p := axiospam.PAMUser{Username: "testana", Password: "thisisatest123"}
-	p.Authenticate()
-	auth, reason := p.IsAuthenticated()
-	fmt.Printf("Person %s Authenticated: %v, Reason: %v\n", p.Username, auth, reason)
+	p := axiospam.New(promptuser.Echo("Enter UserName: "), promptuser.NoEcho("Enter Password: "))
+	if a, r := p.Authenticate(); a {
+		fmt.Printf("Person %s is Authenticated\n", p.Username)
+	} else {
+		fmt.Printf("Persion %s failed to Authenticate because %v\n", p.Username, r)
+	}
 }
